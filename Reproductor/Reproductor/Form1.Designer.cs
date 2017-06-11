6b0bd80a95e364b1e79e6e91eac841d35ae1e52d
Axel Fabián Avilá García <latente2010@hotmail.com>
2017-06-10 19:30:58 -0500

    fondo de patanlla e inicio de librerias

--- Reproductor/Reproductor/Form1.Designer.cs
+++ Reproductor/Reproductor/Form1.Designer.cs
@@ -37,12 +37,21 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.media = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.media)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // minimizar
             // 
-            this.minimizar.Location = new System.Drawing.Point(476, 11);
+            this.minimizar.Location = new System.Drawing.Point(501, 12);
             this.minimizar.Name = "minimizar";
             this.minimizar.Size = new System.Drawing.Size(92, 23);
             this.minimizar.TabIndex = 0;
@@ -51,20 +60,21 @@
             // 
             // cerrar
             // 
-            this.cerrar.Location = new System.Drawing.Point(597, 12);
+            this.cerrar.BackColor = System.Drawing.SystemColors.Control;
+            this.cerrar.Location = new System.Drawing.Point(599, 12);
             this.cerrar.Name = "cerrar";
             this.cerrar.Size = new System.Drawing.Size(85, 22);
             this.cerrar.TabIndex = 1;
             this.cerrar.Text = "cerrar";
-            this.cerrar.UseVisualStyleBackColor = true;
+            this.cerrar.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(383, 82);
+            this.button3.Location = new System.Drawing.Point(356, 82);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.Size = new System.Drawing.Size(113, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Reproducir Todo";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -73,16 +83,16 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(92, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "caratula";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(383, 134);
+            this.button5.Location = new System.Drawing.Point(356, 134);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(86, 23);
+            this.button5.Size = new System.Drawing.Size(113, 23);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "Agregar a Favoritos";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -91,7 +101,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(92, 23);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "Letra";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
@@ -100,23 +110,103 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(155, 34);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
+            this.button7.Text = "Lista de reproduccion";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // media
             // 
             this.media.Enabled = true;
-            this.media.Location = new System.Drawing.Point(684, 312);
+            this.media.Location = new System.Drawing.Point(703, 2);
             this.media.Name = "media";
             this.media.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("media.OcxState")));
             this.media.Size = new System.Drawing.Size(10, 10);
             this.media.TabIndex = 7;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 199);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 53);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 51);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(175, 262);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 49);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(247, 263);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(60, 47);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(323, 265);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(62, 44);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nombre de la cancion";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 333);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(641, 47);
+            this.dataGridView1.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 322);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(699, 392);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.media);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -126,10 +216,12 @@
             this.Controls.Add(this.cerrar);
             this.Controls.Add(this.minimizar);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Reproductor";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.media)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -143,6 +235,14 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private AxWMPLib.AxWindowsMediaPlayer media;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Reproductor/Reproductor/Form1.cs
+++ Reproductor/Reproductor/Form1.cs
@@ -21,5 +21,10 @@ namespace Reproductor
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
