42bc219cd92c7b475b55b7ce19be3b4bdbc20354
Axel Fabián Avilá García <latente2010@hotmail.com>
2017-06-10 22:27:44 -0500

    listas y clases casi terminadas

--- Reproductor/Reproductor/Form1.Designer.cs
+++ Reproductor/Reproductor/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.button10 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.macTrackBar1 = new XComponent.SliderBar.MACTrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.media)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -57,6 +58,7 @@
             this.minimizar.TabIndex = 0;
             this.minimizar.Text = "minimizar";
             this.minimizar.UseVisualStyleBackColor = true;
+            this.minimizar.Click += new System.EventHandler(this.minimizar_Click);
             // 
             // cerrar
             // 
@@ -67,6 +69,7 @@
             this.cerrar.TabIndex = 1;
             this.cerrar.Text = "cerrar";
             this.cerrar.UseVisualStyleBackColor = false;
+            this.cerrar.Click += new System.EventHandler(this.cerrar_Click);
             // 
             // button3
             // 
@@ -76,6 +79,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Reproducir Todo";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -85,6 +89,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "caratula";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -94,6 +99,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "Agregar a Favoritos";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -103,6 +109,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "Letra";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -112,6 +119,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "Lista de reproduccion";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // media
             // 
@@ -138,6 +146,7 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -147,6 +156,7 @@
             this.button2.TabIndex = 10;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button8
             // 
@@ -156,6 +166,7 @@
             this.button8.TabIndex = 11;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -165,6 +176,7 @@
             this.button9.TabIndex = 12;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -174,6 +186,7 @@
             this.button10.TabIndex = 13;
             this.button10.Text = "button10";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // label1
             // 
@@ -193,12 +206,35 @@
             this.dataGridView1.Size = new System.Drawing.Size(641, 47);
             this.dataGridView1.TabIndex = 15;
             // 
+            // macTrackBar1
+            // 
+            this.macTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.macTrackBar1.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.macTrackBar1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.macTrackBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(125)))), ((int)(((byte)(123)))));
+            this.macTrackBar1.IndentHeight = 6;
+            this.macTrackBar1.Location = new System.Drawing.Point(373, 224);
+            this.macTrackBar1.Maximum = 10;
+            this.macTrackBar1.Minimum = 0;
+            this.macTrackBar1.Name = "macTrackBar1";
+            this.macTrackBar1.Size = new System.Drawing.Size(328, 47);
+            this.macTrackBar1.TabIndex = 16;
+            this.macTrackBar1.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(146)))), ((int)(((byte)(148)))));
+            this.macTrackBar1.TickHeight = 4;
+            this.macTrackBar1.TrackerColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(130)))), ((int)(((byte)(198)))));
+            this.macTrackBar1.TrackerSize = new System.Drawing.Size(16, 16);
+            this.macTrackBar1.TrackLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(93)))), ((int)(((byte)(90)))));
+            this.macTrackBar1.TrackLineHeight = 3;
+            this.macTrackBar1.TrackLineSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(93)))), ((int)(((byte)(90)))));
+            this.macTrackBar1.Value = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(699, 392);
+            this.Controls.Add(this.macTrackBar1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button10);
@@ -215,6 +251,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.cerrar);
             this.Controls.Add(this.minimizar);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Reproductor";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -243,6 +280,7 @@
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private XComponent.SliderBar.MACTrackBar macTrackBar1;
     }
 }
 
--- Reproductor/Reproductor/Form1.cs
+++ Reproductor/Reproductor/Form1.cs
@@ -7,13 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Reproductor
 {
     public partial class Form1 : Form
     {
+        List<reproduciendo> listareproduciendo = new List<reproduciendo>();
+        List<biblio> listabiblio = new List<biblio>();
+        List<mp3> listamp3 = new List<mp3>();
         public Form1()
         {
+            
             InitializeComponent();
         }
 
@@ -25,6 +33,339 @@ namespace Reproductor
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+        public void tag(string dato)
+        {
+            string dat = dato;
+            TagLib.File file = TagLib.File.Create(dat);
+            System.Drawing.Image currentImage = null;
+
+            // In method onclick of the listbox showing all mp3's
+
+            if (file.Tag.Pictures.Length > 0)
+            {
+                TagLib.IPicture pic = file.Tag.Pictures[0];
+                MemoryStream ms = new MemoryStream(pic.Data.Data);
+                if (ms != null && ms.Length > 4096)
+                {
+                    currentImage = System.Drawing.Image.FromStream(ms);
+                    // Load thumbnail into PictureBox
+                    caratula.Image = currentImage.GetThumbnailImage(200, 200, null, System.IntPtr.Zero);
+                }
+                ms.Close();
+            }
+
+            mp3 datmp = new mp3();
+           
+            datmp.Nombrecancion = file.Tag.Title;
+            datmp.Numero = Convert.ToString(file.Tag.Track);
+            datmp.Album = file.Tag.Album;
+            datmp.Año = Convert.ToString(file.Tag.Year);
+            datmp.Tipo = file.Tag.FirstGenre;
+            datmp.Tiempo = file.Properties.Duration.ToString();
+            datmp.Artista = file.Tag.FirstArtist;
+            datmp.Notas = file.Tag.Comment;
+            listamp3.Add(datmp);
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = listamp3;
+            dataGridView1.Refresh();
+        }
+        private void Form1_load(object sender, EventArgs e)
+        {
+            media.uiMode = "invisible";
+            listBox1.Visible = false;
+            caratula.Visible = true;
+        }
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                media.URL = openFileDialog1.FileName;
+            }
+            listamp3.RemoveRange(0, listamp3.Count);
+            media.Ctlcontrols.play();
+            tag(openFileDialog1.FileName);
+            label1.Text = openFileDialog1.Title;
+
+        }
+        public void actualizar2()
+        {
+            if (listabiblio.Count == 0)
+            {
+                leerbiblio();
+            }
+
+        }
+        public void actualizar()
+        {
+            if (listareproduciendo.Count == 0)
+            {
+                leerxml();
+            }
+         }
+        public void leerbiblio()
+        {
+            XDocument documento = XDocument.Load(@"biblio.xml");
+            var listar = from lis in documento.Descendants("Blibioteca") select lis;
+            foreach (XElement u in listar.Elements("Cancion"))
+            {
+                biblio tmp = new biblio();
+                tmp.Nombre = u.Element("Nombre").Value;
+                tmp.Direccion = u.Element("Direccîón").Value;
+                tmp.Numero = u.Element("No").Value;
+                tmp.Album = u.Element("Album").Value;
+                tmp.Tiempo = u.Element("Tiempo").Value;
+                tmp.Condicion = u.Element("Calidad").Value;
+
+                listabiblio.Add(tmp);
+
+            }
+        }
+        public void leerxml()
+        {
+            XDocument documento = XDocument.Load(@"miXML.xml");
+            var listar = from lis in documento.Descendants("Lista_Favoritos") select lis;
+            foreach (XElement u in listar.Elements("Cancion"))
+            {
+                reproduciendo tmp = new reproduciendo();
+                tmp.Nombre = u.Element("Titulo").Value;
+                tmp.Direccion = u.Element("Url").Value;
+                listareproduciendo.Add(tmp);
+
+            }
+        }
+        private void macTrackBar1_ValueChanged(object sender, decimal value)
+        {
+
+            media.settings.volume = macTrackBar1.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            macTrackBar1.Value = media.settings.volume;
+            actualizar();
+
+        }
+        public void elminarlisderepro()
+        {
+            XmlDocument documento = new XmlDocument();
+            string ruta = @"miXML.xml";
+            //Cargamos el documento XML.
+            documento = new XmlDocument();
+            documento.Load(ruta);
+            //Obtenemos el nodo raiz del documento.
+            XmlElement bibliot = documento.DocumentElement;
+
+            //Obtenemos la lista de todos los empleados.
+            XmlNodeList listacancion = documento.SelectNodes("Lista_Favoritos/Cancion");
+
+            foreach (XmlNode item in listacancion)
+            {
+                for (int i = 0; i < listareproduciendo.Count; i++)
+                {
+                    //Determinamos el nodo a modificar por medio del id de empleado.
+                    if (item.FirstChild.InnerText == listareproduciendo[i].Nombre)
+                    {
+                        //Nodo sustituido.
+                        XmlNode nodoOld = item;
+                        bibliot.RemoveChild(nodoOld);
+                    }
+                }
+
+                //Salvamos el documento.
+                documento.Save(ruta);
+            }
+        }
+
+        private void minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+       
+
+        private void cerrar_Click(object sender, EventArgs e)
+        {
+            elminarlisderepro();
+            Application.ExitThread();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listareproduciendo.RemoveRange(0, listareproduciendo.Count);
+            actualizar();
+            var myPlayList = media.playlistCollection.newPlaylist("MyPlayList");
+
+            for (int i = 0; i < listareproduciendo.Count; i++)
+            {
+                var mediaItem = media.newMedia(listareproduciendo[i].Direccion);
+                myPlayList.appendItem(mediaItem);
+            }
+            media.currentPlaylist = myPlayList;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = false;
+            caratula.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = true;
+            caratula.Visible = false;
+            this.listBox1.Items.Clear();
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Archivo txt (*.txt)|*.txt|All(*,*)|*,*";
+            try
+            {
+                open.ShowDialog();
+                StreamReader import = new StreamReader(Convert.ToString(open.FileName));
+                while (import.Peek() >= 0)
+                {
+                    listBox1.Items.Add(Convert.ToString(import.ReadLine()));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex.Message));
+                return;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //this.Hide(); //cerrar formulario actual
+            media.Ctlcontrols.stop();
+            this.Hide();
+            listaderepro frm = new listaderepro();
+            frm.Hide();
+            frm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listareproduciendo.RemoveRange(0, listareproduciendo.Count);
+            actualizar();
+            int max = listareproduciendo.Count;
+            for (int i = 0; i < listareproduciendo.Count; i++)
+            {
+                if (label1.Text == listareproduciendo[i].Nombre)
+                {
+                    if (i == 0)
+                    {
+                        media.URL = listareproduciendo[max - 1].Direccion;
+                        label1.Text = listareproduciendo[max - 1].Nombre;
+                        break;
+                    }
+                    else
+                    {
+                        media.URL = listareproduciendo[i - 1].Direccion;
+                        label1.Text = listareproduciendo[i - 1].Nombre;
+                        break;
+                    }
+
+                }
+            }
+            listamp3.RemoveRange(0, listamp3.Count);
+            tag(media.URL);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (media.URL == "")
+            {
+                if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    media.URL = openFileDialog1.FileName;
+                }
+
+                media.Ctlcontrols.play();
+                tag(openFileDialog1.FileName);
+                label1.Text = openFileDialog1.Title;
+
+
+            }
+
+
+            else {
+                media.Ctlcontrols.play();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            media.Ctlcontrols.pause();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            media.Ctlcontrols.stop();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (listareproduciendo.Count == 0)
+            {
+                listabiblio.RemoveRange(0, listabiblio.Count);
+                actualizar2();
+                int max2 = listabiblio.Count;
+                for (int i = 0; i < listabiblio.Count; i++)
+                {
+                    if (label1.Text == listabiblio[i].Nombre)
+                    {
+                        if (i == max2 - 1)
+                        {
+                            media.URL = listabiblio[0].Direccion;
+                            label1.Text = listabiblio[0].Nombre;
+                            break;
+                        }
+                        else
+                        {
+                            media.URL = listabiblio[i + 1].Direccion;
+                            label1.Text = listabiblio[i + 1].Nombre;
+                            break;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                listareproduciendo.RemoveRange(0, listareproduciendo.Count);
+                actualizar();
+                int max = listareproduciendo.Count;
+                for (int i = 0; i < listareproduciendo.Count; i++)
+                {
+                    if (label1.Text == listareproduciendo[i].Nombre)
+                    {
+                        if (i == max - 1)
+                        {
+                            media.URL = listareproduciendo[0].Direccion;
+                            label1.Text = listareproduciendo[0].Nombre;
+                            break;
+                        }
+                        else
+                        {
+                            media.URL = listareproduciendo[i + 1].Direccion;
+                            label1.Text = listareproduciendo[i + 1].Nombre;
+                            break;
+                        }
+
+                    }
+                }
+
+            }
+            media.Ctlcontrols.play();
+            listamp3.RemoveRange(0, listamp3.Count);
+            tag(media.URL);
         }
     }
 }
